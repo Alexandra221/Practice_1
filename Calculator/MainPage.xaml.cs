33f90cdb3d7c427b5701f3ec1c54ba3810ff2498
Александра Аброськина <60029457+Alexandra221@users.noreply.github.com>
2021-05-27 13:44:28 +0300

    мобильное приложение

---
+++ Calculator/MainPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Calculator
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        public void Submit_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                double first = Convert.ToDouble(First.Text);
+                double result = first * 1.76;
+                Page1 secondpage = new Page1();
+                Navigation.PushAsync(secondpage);
+                secondpage.Calculator(result);
+            }
+            catch (FormatException)
+            {
+                DisplayAlert("Ошибка", "Введите числовые данные", "Ввести заново");
+
+            }
+
+
+        }
+    }
+
+}
